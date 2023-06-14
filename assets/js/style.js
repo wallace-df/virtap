c04828621011eaa8580bb6f0a33b212e00ba1566
Wallace Ferreira <54377956+wallace-df@users.noreply.github.com>
2023-06-13 22:00:25 -0300

    Add files via upload

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,49 +1,92 @@
-// Set the target date for the countdown
-const targetDate = new Date('2023-06-01');
+window.signupAPIEndpoint = 'https://api.virtap.com.br';
 
-// Update the countdown every second
-function updateCountdown() {
-  const currentDate = new Date();
-  const timeDifference = targetDate - currentDate;
-
-  // Calculate remaining time in days, hours, minutes, and seconds
-  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-
-  // Display the remaining time in the HTML elements
-  document.getElementById('days').textContent = formatTime(days);
-  document.getElementById('hours').textContent = formatTime(hours);
-  document.getElementById('minutes').textContent = formatTime(minutes);
-  document.getElementById('seconds').textContent = formatTime(seconds);
+function popupWindow(url, windowName, win, w, h) {
+  const y = win.top.outerHeight / 2 + win.top.screenY - (h / 2);
+  const x = win.top.outerWidth / 2 + win.top.screenX - (w / 2);
+  return win.open(url, windowName, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`);
 }
 
-// Format time values to always have two digits (e.g., 01, 02, ..., 10, 11, ...)
-function formatTime(time) {
-  return time < 10 ? time : time;
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-// Initial call to update the countdown
-updateCountdown();
 
-// Start updating the countdown every second
-setInterval(updateCountdown, 1000);
+$(document).ready(function () {
+  let nome = getParameterByName('nome');
 
-// Navbar
-function toggle() {
-  var content = document.getElementById("navbarNav");
-  if (content.style.display === "none") {
-    content.style.display = "block";
-  } else {
-    content.style.display = "none";
+  if (nome) {
+    $("#txtNome").val(nome);
   }
-}
+});
 
-// Upload Image
-function upload() {
-  var imgcanvas = document.getElementById("canv1");
-  var fileinput = document.getElementById("finput");
-  var image = new SimpleImage(fileinput);
-  image.drawTo(imgcanvas);
+function signup() {
+  let $txtName = $("#txtNome");
+  let $txtWhatsapp = $("#txtWhatsapp");
+  let $chkTermos = $("#chkTermos");
+  let $txtError = $("#txtError");
+
+  $txtError.hide();
+  $txtName.closest('.item').removeClass('error');
+  $txtWhatsapp.closest('.item').removeClass('error');
+  $chkTermos.closest('.item').removeClass('error');
+
+  let hasError = false;
+
+  if ($txtName.val().length < 5) {
+    $txtName.closest('.item').addClass('error');
+    hasError = true;
+  }
+
+  if ($txtWhatsapp.val().length < 8) {
+    $txtWhatsapp.closest('.item').addClass('error');
+    hasError = true;
+  }
+
+  if (!$chkTermos.is(":checked")) {
+    $chkTermos.closest('.item').addClass('error');
+    hasError = true;
+  }
+
+  if (!hasError) {
+    $("#signupForm").find('input, .btn').prop("disabled", true).addClass("disabled");
+    $("#btnSignup").text("Por favor, aguarde...");
+
+    let formData = new FormData();
+    formData.append('name', $txtName.val());
+    formData.append('contact_info', JSON.stringify({ whatsapp: $txtWhatsapp.val() }));
+
+    $.ajax({
+      url: window.signupAPIEndpoint + '/client/register-profile',
+      data: formData,
+      processData: false,
+      contentType: false,
+      type: 'POST',
+      xhrFields: {
+        withCredentials: true
+      },
+      error: function (err) {
+        $("#signupForm").find('input, .btn').prop("disabled", false).removeClass("disabled");
+        $("#btnSignup").text("Cadastrar");
+        if (err.status === 401) {
+          $txtError.html("Sua sessão expirou.<br/> <a class='link' href='javascript:void(0)' onclick='popupWindow(\"" + window.signupAPIEndpoint + "/auth/google/client?popup=true\",\"_blank\",window, 600, 400)' target='_blank' class='link'> Clique aqui para entrar novamente.</a>").show();
+        } else {
+          $txtError.text("Ocorreu um erro no seu cadastro. Por favor, tente novamente.").show();
+
+        }
+      },
+      success: function () {
+        window.location = 'https://clientes.virtap.com.br';
+      }
+    });
+
+
+  }
+
 }
