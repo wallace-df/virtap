b60e799b3bfc4f497cba05e1a5cdccfa256610cb
wallace.d.f@hotmail.com <wallaceferreira19@gmail.com>
2023-06-23 15:28:43 -0300

    Fixes.

--- assistente-virtual/cadastro/js/functions-v1.js
+++ assistente-virtual/cadastro/js/functions-v1.js
@@ -1,10 +1,23 @@
 window.signupAPIEndpoint = 'https://api.virtap.com.br';
-window.clientDashboard = 'https://clientes.virtap.com.br';
+window.assistantDashboard = 'https://assistentes.virtap.com.br';
 
 function popupWindow(url, windowName, win, w, h) {
   const y = win.top.outerHeight / 2 + win.top.screenY - (h / 2);
   const x = win.top.outerWidth / 2 + win.top.screenX - (w / 2);
   return win.open(url, windowName, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`);
+}
+
+function upload() {
+  var fileinput = document.getElementById("finput");
+  if (fileinput.files.length > 0) {
+    let reader = new FileReader();
+    reader.onload = function (event) {
+      $("#canv1").attr('src', event.target.result);
+    };
+    reader.readAsDataURL(fileinput.files[0]);
+  } else {
+    $("#canv1").removeAttr('src');
+  }
 }
 
 function getParameterByName(name, url) {
@@ -17,7 +30,6 @@ function getParameterByName(name, url) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-
 $(document).ready(function () {
   let nome = getParameterByName('nome');
 
@@ -27,25 +39,46 @@ $(document).ready(function () {
 });
 
 function signup() {
+  let $filePicture = $("#finput");
   let $txtName = $("#txtNome");
-  let $txtWhatsapp = $("#txtWhatsapp");
+  let $txtTitle = $("#txtTitle");
+  let $txtDescription = $("#txtDescription");
+  let $chkAreas = $("#areasAtuacao");
   let $chkTermos = $("#chkTermos");
   let $txtError = $("#txtError");
 
   $txtError.hide();
   $txtName.closest('.item').removeClass('error');
-  $txtWhatsapp.closest('.item').removeClass('error');
+  $txtTitle.closest('.item').removeClass('error');
+  $chkAreas.closest('.item').removeClass('error');
+  $txtDescription.closest('.item').removeClass('error');
   $chkTermos.closest('.item').removeClass('error');
 
   let hasError = false;
 
-  if ($txtName.val().length < 10) {
+  if ($txtName.val().length < 5) {
     $txtName.closest('.item').addClass('error');
     hasError = true;
   }
 
-  if ($txtWhatsapp.val().length < 10) {
-    $txtWhatsapp.closest('.item').addClass('error');
+  if ($txtTitle.val().length < 8) {
+    $txtTitle.closest('.item').addClass('error');
+    hasError = true;
+  }
+
+  if ($txtDescription.val().length < 50) {
+    $txtDescription.closest('.item').addClass('error');
+    hasError = true;
+  }
+
+  let selectedAreas = [];
+
+  $chkAreas.find('input[type=checkbox]:checked').each(function (i, e) {
+    selectedAreas.push($(e).attr('value'));
+  });
+
+  if (selectedAreas.length === 0 || selectedAreas.length > 3) {
+    $chkAreas.closest('.item').addClass('error');
     hasError = true;
   }
 
@@ -60,10 +93,16 @@ function signup() {
 
     let formData = new FormData();
     formData.append('name', $txtName.val());
-    formData.append('contact_info', JSON.stringify({ whatsapp: $txtWhatsapp.val() }));
+    formData.append('title', $txtTitle.val());
+    formData.append('expertise_areas', JSON.stringify(selectedAreas));
+    formData.append('description', $txtDescription.val());
+
+    if ($filePicture[0].files.length > 0) {
+      formData.append('picture_file', $filePicture[0].files[0]);
+    }
 
     $.ajax({
-      url: window.signupAPIEndpoint + '/client/register-profile',
+      url: window.signupAPIEndpoint + '/assistant/register-profile',
       data: formData,
       processData: false,
       contentType: false,
@@ -75,12 +114,12 @@ function signup() {
         if (xhr.status === 401) {
           $("#signupForm").find('input, .btn').prop("disabled", false).removeClass("disabled");
           $("#btnSignup").text("Cadastrar");
-          $txtError.html("Sua sessão expirou.<br/> <a class='link' href='javascript:void(0)' onclick='popupWindow(\"" + window.signupAPIEndpoint + "/auth/google/client?popup=true\",\"_blank\",window, 600, 400)' target='_blank' class='link'> Clique aqui para entrar novamente.</a>").show();
+          $txtError.html("Sua sessão expirou.<br/> <a class='link' href='javascript:void(0)' onclick='popupWindow(\"" + window.signupAPIEndpoint + "/auth/google/assistant?popup=true\",\"_blank\",window, 600, 400)' target='_blank' class='link'> Clique aqui para entrar novamente.</a>").show();
         } else {
           try {
             let err = JSON.parse(xhr.responseText);
-            if (err.errorCode === 'CLIENT_ALREADY_REGISTERED') {
-              window.location = clientDashboard;
+            if (err.errorCode === 'ASSISTANT_ALREADY_REGISTERED') {
+              window.location = assistantDashboard;
               return;
             }
             throw xhr;
@@ -93,7 +132,7 @@ function signup() {
         }
       },
       success: function () {
-        window.location = clientDashboard;
+        window.location = assistantDashboard;
       }
     });
 
