a3348d8e5277735974f6df141a07d708c9b92dd4
wallace.d.f@hotmail.com <wallaceferreira19@gmail.com>
2024-07-09 01:05:12 -0300

    Fixes.

--- js/assinar-v1.js
+++ js/assinar-v1.js
@@ -1,37 +1,32 @@
 window.signupAPIEndpoint = 'http://localhost:3000';
 window.assistantDashboard = 'https://assistentes.virtap.com.br';
-const stripe = Stripe('pk_test_51NpbOCC7jqja7iX3kgJo7g8FcCBti8YURXCIaGIsBlNBlV31g9jylf1op2Vb4mUjMsVwEtdcBrrLJQK8zfhXj6Eo00UcoGEzPF');
-
-
-const handleServerResponse = async (response) => {
-  if (response.errorCode) {
-    console.log(response);
-    alert('err1');
-  } else if (response.status === "requires_action") {
-    // Use Stripe.js to handle the required next action
-    const {
-      error,
-      paymentIntent
-    } = await stripe.handleNextAction({
-      clientSecret: response.clientSecret
-    });
-
-    if (error) {
-      console.log(error);
-      alert('err2');
-
-      // Show error from Stripe.js in payment form
-    } else {
-      alert('good 1!')
-      // Actions handled, show success message
-    }
-  } else {
-    alert('good 2!')
-    // No actions needed, show success message
-  }
-}
 
 let formData = new FormData();
+
+
+
+async function handleError(response) {
+  console.log(response);
+  let showGenericError = true;
+  if (response.errorCode === 'ALREADY_SUBSCRIBED') {
+    if (window.target_plan === 'Basic' && response.errorData.assistant_plan === 'Basic') {
+      $("#loading").html('<div><h1>Você já assinou o plano Basic.</h1><br /><p>Redirecionando automaticamente...</p></div>');
+      showGenericError = false;
+    } else if (response.errorData.assistant_plan === 'Vip') {
+      $("#loading").html('<div><h1>Você já assinou o plano Vip.</h1><br /><p>Redirecionando automaticamente...</p></div>');
+      showGenericError = false;
+    } else {
+      console.log("Invalid assistant plan:", response.errorData.assistant_plan);
+    }
+  }
+
+  $("#loading").show();
+  $("#sign_up").hide();
+
+  if (showGenericError) {
+    $("#loading").html('<h1 style="color: #f66666">Ocorreu um erro.<br/><br/> Por favor, atualize a página e tente novamente.</h1>');
+  }
+}
 
 $.ajax({
   url: window.signupAPIEndpoint + '/subscribe?plan=' + window.target_plan,
@@ -42,31 +37,15 @@ $.ajax({
   xhrFields: {
     withCredentials: true
   },
-  error: function (xhr) {
-    let showError = true;
+  error: async function (xhr) {
     if (xhr.status === 400 && xhr.responseJSON) {
-      if (xhr.responseJSON.errorCode === 'ALREADY_SUBSCRIBED') {
-        if (window.target_plan === 'Starter' && xhr.responseJSON.errorData.plan === 'Starter') {
-          showError = false;
-          $("#loading").html('<h1>Você já assinou o plano Starter.</h1><br /><p>Redirecionando automaticamente...</p>');
-        } else if (window.target_plan === 'Vip' && xhr.responseJSON.errorData.plan === 'Vip') {
-          showError = false;
-          $("#loading").html('<h1>Você já assinou o plano Vip.</h1><br /><p>Redirecionando automaticamente...</p>');
-        } else if (xhr.responseJSON.errorData.billingSession !== null) {
-          showError = false;
-          $("#loading").html('<h1>Você já assina um plano na Virtap.</h1><br /><p>Vamos te redirecionar para página de alteração de planos...</p>');
-          setTimeout(() => window.location = xhr.responseJSON.errorData.billingSession, 5000);
-        }
-      }
-    }
-    if (showError) {
-      $("#loading").html('<h1 style="color: #f66666">Ocorreu um erro.<br/><br/> Por favor, atualize a página e tente novamente.</h1>');
+      await handleError(xhr.responseJSON);
     }
   },
   success: function (response) {
     response = response.responseData;
 
-    $("#loading").remove();
+    $("#loading").hide();
     $("#sign_up").show();
     $("#cpf_cnpj").val(response.cpf_cnpj);
     $("#email").val(response.email);
@@ -77,13 +56,6 @@ $.ajax({
 
     const submitBtn = document.getElementById('submit-btn');
 
-    const handleError = (error) => {
-      const messageContainer = document.querySelector('#error-message');
-      console.log(error);
-      messageContainer.textContent = error.message;
-      $("#submit-btn").prop('disabled', false);
-
-    }
 
     $("#submit-btn").on('click', async (event) => {
       // We don't want to let default form submission happen here,
@@ -96,44 +68,17 @@ $.ajax({
       }
 
       // Disable form submission while loading
-      $("#submit-btn").prop('disabled', true);
-
-      // Trigger form validation and wallet collection
-      const { error: submitError } = await elements.submit();
-      if (submitError) {
-        handleError(submitError);
-        return;
-      }
+      $("#submit-btn").prop('disabled', true).text('Por favor, aguarde...');
 
       // Create the subscription
       try {
 
-        let addressObj = await addressElement.getValue();
-
-        // Create the ConfirmationToken using the details collected by the Payment Element
-        // and additional shipping information
-        const { error, confirmationToken } = await stripe.createConfirmationToken({
-          elements,
-          params: {
-            return_url: 'https://example.com/order/123/complete'
-          }
-        });
-
-        if (error) {
-          // This point is only reached if there's an immediate error when
-          // creating the ConfirmationToken. Show the error to your customer (for example, payment details incomplete)
-          handleError(error);
-          return;
-        }
 
         let formData = new FormData();
         formData.append("target_plan", window.target_plan);
-        formData.append("name", addressObj.value.name);
+        formData.append("name", $("#name").val());
         formData.append("email", $("#email").val());
         formData.append("cpf_cnpj", $("#cpf_cnpj").val());
-        formData.append("confirmationToken", confirmationToken.id);
-        formData.append("address", JSON.stringify(addressObj.value.address));
-        formData.append("phone", addressObj.value.phone);
 
         // Create the PaymentIntent
         const res = await fetch("http://localhost:3000/subscribe", {
@@ -142,11 +87,12 @@ $.ajax({
           body: formData
         });
 
+        if (res.status !== 200) {
+          throw await res.json();
+        }
+
         const data = await res.json();
-
-        // Handle any next actions or errors. See the Handle any next actions step for implementation.
-        handleServerResponse(data);
-
+        console.log(data);
 
       } catch (err) {
         handleError(err);
