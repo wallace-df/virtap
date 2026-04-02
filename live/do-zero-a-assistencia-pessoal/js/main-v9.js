d9c6bfa21cb353197082b01d74a637d6d1aac7ec
wallace <wallaceferreira19@gmail.com>
2026-04-02 11:31:35 -0300

    Fix.

--- live/do-zero-a-assistencia-pessoal/js/main-v9.js
+++ live/do-zero-a-assistencia-pessoal/js/main-v9.js
@@ -58,7 +58,7 @@ function collectFormData() {
  
 function goToGroup() {
     
-    let url = 'https://chat.whatsapp.com/K7QAMjDWqaJBI7M7RBK0pc';
+    let url = 'https://chat.whatsapp.com/Cevacm7edGd7ZGYSQQevwU';
     window.dataLayer.push({
         'event': 'ga_event',
         'ga_event_name': 'go_to_whatsapp_page',
